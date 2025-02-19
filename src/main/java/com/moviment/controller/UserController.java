4fccb0433025ec08cb95746a4eaab553c40cde90
Seunghyub Choi <seunghyub.choi@gmail.com>
2025-02-19 20:40:11 +0900

    valid added

--- src/main/java/com/moviment/controller/UserController.java
+++ src/main/java/com/moviment/controller/UserController.java
@@ -2,13 +2,14 @@ package com.moviment.controller;
 
 import com.moviment.model.UserVO;
 import com.moviment.service.UserService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
