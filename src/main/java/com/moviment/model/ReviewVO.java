e321bd1ac45b96868d6e96d3a48db916233c5ae6
Seunghyub Choi <seunghyub.choi@gmail.com>
2025-03-04 21:08:10 +0900

    ReviewVO

---
+++ src/main/java/com/moviment/model/ReviewVO.java
@@ -0,0 +1,16 @@
+package com.moviment.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ReviewVO {
+    private int id;
+    private String userId;
+    private String movieId;
+    private String content;
+    private String createdAt;
+}
